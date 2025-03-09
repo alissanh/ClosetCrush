ee6fe61a1aed6b625f5639bce0a468c3c60590e1
alissanh <alissahuynh004@gmail.com>
2025-03-08 18:02:36 -0800

    dotenv fixed

--- main/data.js
+++ main/data.js
@@ -1,22 +1,6 @@
 import mongoose from 'mongoose';
 
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/my-database';
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
-
 // Each user has arrays for tops, bottoms, shoes, accessories
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
